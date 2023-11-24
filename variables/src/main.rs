56bc69beb60ce925492ce2d86cd2f515ad82b215
Fadhli Rahim <fadhlirahim@gmail.com>
2023-11-24 17:24:55 +0800

    tuples and arrays

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -56,6 +56,38 @@ fn main() {
     println!("c: {}", c);
     println!("z: {}", z);
     println!("heart_eyed_cat: {}", heart_eyed_cat);
+
+    // tuples
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+
+    println!("x: {}", x);
+    println!("y: {}", y);
+    println!("z: {}", z);
+    
+    let f: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = f.0;
+    println!("five_hundred: {}", five_hundred);
+
+    let six_point_four = f.1;
+    println!("six_point_four: {}", six_point_four);
+
+    let one = f.2;
+    println!("one: {}", one);
+
+
+    // arrays
+    let a = [1, 2, 3, 4, 5];
+    println!("a: {:?}", a);
+
+    let first = a[0];
+    let second = a[1];
+    println!("first, second: {}, {}", first, second);
+    
+    // intentionally inducing a runtime error
+    let index = 10;
+    let element = a[index];
+    println!("the value of element is: {}", element);
 }
 
 
