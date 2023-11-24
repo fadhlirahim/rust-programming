4fe4121aca1455c8b54e743d221c79c740e506df
Fadhli Rahim <fadhlirahim@gmail.com>
2023-11-24 16:59:57 +0800

    update variables types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -10,11 +10,52 @@ fn main() {
 
     let guess: u32 = "42".parse().expect("Not a number!");
     println!("the value of guess is: {}", guess);
-
+    
+    // floating point
     let x = 2.0;
     let y: f32 = 3.1;
 
     println!("x + y = {}", x + y);
+
+
+    // Numeric Operations
+    
+    // additions
+    let sum = 5 + 10;
+    println!("sum is: {}", sum);
+
+    // subtractions
+    let difference = 95.5 - 4.3;
+    println!("difference is: {}", difference);
+
+    // multiplications
+    let product = 4 * 30;
+    println!("product is: {}", product);
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("quotient is: {}", quotient);
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("remainder is: {}", remainder);
+
+
+    // boolean
+    let t = true;
+    println!("{}", t);
+
+    let f: bool = false;
+    println!("f: {}", f);
+
+    // Characters
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    println!("c: {}", c);
+    println!("z: {}", z);
+    println!("heart_eyed_cat: {}", heart_eyed_cat);
 }
 
 
