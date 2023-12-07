f857a2de9bc3a2b836f2cfe7d56402798d18740b
Fadhli Rahim <fadhlirahim@gmail.com>
2023-12-07 22:48:50 +0800

    update rustyy

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+
+    // Using mutable, growable piece of text
+    let s1 = String::from("hello"); 
+    let s2 = s1; 
+
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
 }
