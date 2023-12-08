c35cf133e1ad4b08dd6d35ace90297d065309f44
Fadhli Rahim <fadhlirahim@gmail.com>
2023-12-08 17:13:02 +0800

    slice

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     // Using mutable, growable piece of text
     let s1 = String::from("hello"); 
     let s2 = &s1; // no problem with immutable reference
-    // let s3 = &mut s1; // big  problem
+    // let s3 = &mut s1; // big problem
 
     println!("s1 = {}, s2 = {}", s1, s2);
 
@@ -16,6 +16,13 @@ fn main() {
     let hello = String::from("bye, hello");
     let first_word = first_word(&hello);
     println!("first_word = {}", first_word);
+
+    // Some slice examples
+    let numbers = [1, 2, 3, 4, 5];
+
+    let slice = &numbers[1..];
+
+    println!("{:?}", slice);
 }
 
 
