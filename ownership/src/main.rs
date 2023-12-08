cde048d7c6b83c3f72867be1dbff158c5bb0932a
Fadhli Rahim <fadhlirahim@gmail.com>
2023-12-08 17:10:12 +0800

    update rust ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -2,7 +2,8 @@ fn main() {
 
     // Using mutable, growable piece of text
     let s1 = String::from("hello"); 
-    let s2 = &s1; 
+    let s2 = &s1; // no problem with immutable reference
+    // let s3 = &mut s1; // big  problem
 
     println!("s1 = {}, s2 = {}", s1, s2);
 
@@ -10,4 +11,24 @@ fn main() {
     let y = x;
 
     println!("x = {}, y = {}", x, y);
+
+
+    let hello = String::from("bye, hello");
+    let first_word = first_word(&hello);
+    println!("first_word = {}", first_word);
 }
+
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+
