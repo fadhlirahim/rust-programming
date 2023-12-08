b8c6938fcf80b9757669565f77cc48927654637b
Fadhli Rahim <fadhlirahim@gmail.com>
2023-12-08 09:13:19 +0800

    update code examples

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,13 +1,3 @@
 fn main() {
-
-    // Using mutable, growable piece of text
-    let s1 = String::from("hello"); 
-    let s2 = s1; 
-
-    println!("s1 = {}, s2 = {}", s1, s2);
-
-    let x = 5;
-    let y = x;
-
-    println!("x = {}, y = {}", x, y);
+    println!("Hello, world!");
 }
